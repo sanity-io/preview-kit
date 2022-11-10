b81b69bbd368e659cdb43c8285292aa283cc7bef
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-11-10 18:08:56 +0100

    chore: remove todo warning

--- package.config.ts
+++ package.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@sanity/pkg-utils'
 
 export default defineConfig({
+  // eslint-disable-next-line no-warning-comments
   // @TODO turn minify back on after verifying that everything works well
   minify: false,
 })
