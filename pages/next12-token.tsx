89e9508e634fd033e626427571088f55fd514a3c
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-11-11 03:02:21 +0100

    chore: update token demo

--- pages/next12-token.tsx
+++ pages/next12-token.tsx
@@ -1,36 +1,81 @@
+import { PreviewSuspense } from '@sanity/preview-kit'
 import Container from 'app/Container'
 import PreviewButton from 'app/PreviewButton'
-import type { GetStaticProps } from 'next'
+import {
+  type TableProps,
+  query as tableQuery,
+  Table,
+  TableFallback,
+} from 'app/Table'
+import { createClient } from 'app/utils'
+import type { GetStaticProps, InferGetStaticPropsType } from 'next'
+import { lazy } from 'react'
+
+const PreviewTable = lazy(() => import('pages-extra/PreviewTable'))
+
+export const getStaticProps: GetStaticProps<{
+  preview: boolean
+  token?: string
+  tableData: TableProps['data']
+  footerData: unknown
+}> = async ({ preview = false, previewData = {} }) => {
+  const revalidate = 60
+
+  if (preview) {
+    return {
+      props: {
+        preview,
+        token: (previewData as any)?.token,
+        tableData: [],
+        footerData: null,
+      },
+      revalidate,
+    }
+  }
+
+  const client = createClient()
+  const tablePromise = client.fetch(tableQuery)
+
+  return {
+    props: {
+      preview,
+      tableData: await tablePromise,
+      footerData: null,
+    },
+    revalidate,
+  }
+}
 
 export default function Next12TokenPage({
   preview,
   token,
-}: {
-  preview?: boolean
-  token?: string
-}) {
+  tableData,
+  footerData,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   // eslint-disable-next-line no-console
-  console.log('Next12TokenPage', { preview, token })
+  console.log('Next12TokenPage', { preview, token, footerData })
   return (
     <Container>
       <PreviewButton
-        preview={!!preview}
+        preview={!!preview && !!token}
         start="/api/preview-next12-token"
         stop="/api/exit-preview-next12-token"
       />
+      {preview && token ? (
+        <PreviewSuspense
+          fallback={
+            <>
+              <TableFallback />
+            </>
+          }
+        >
+          <PreviewTable token={token} />
+        </PreviewSuspense>
+      ) : (
+        <>
+          <Table data={tableData} />
+        </>
+      )}
     </Container>
   )
-}
-
-export const getStaticProps: GetStaticProps<any, any, any> = ({
-  preview = false,
-  previewData = {},
-}) => {
-  return {
-    props: {
-      preview,
-      token: previewData?.token || null,
-    },
-    revalidate: 60,
-  }
 }
