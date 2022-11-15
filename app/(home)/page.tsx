93c0d7445df2abb5421311e654c9fcd9ff237a19
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-11-15 23:48:34 +0100

    chore: link to remix demo

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,6 +5,8 @@ const links = [
   ['Next 12 - token', '/next12-token'],
   ['Next 13 - cookie', '/next13-cookie'],
   ['Next 13 - token', '/next13-token'],
+  ['Remix - cookie', 'https://preview-kit-remix.sanity.build/remix-cookie'],
+  ['Remix - token', 'https://preview-kit-remix.sanity.build/remix-token'],
 ]
 // eslint-disable-next-line no-warning-comments
 // @TODO investigate why the Edge runtime routes fail in production
