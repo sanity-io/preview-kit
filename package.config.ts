ae62f9e9982f36f983ac99e519dac8004389889f
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-11-16 19:12:30 +0100

    chore: document why minify can't be enabled

--- package.config.ts
+++ package.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@sanity/pkg-utils'
 
 export default defineConfig({
   // eslint-disable-next-line no-warning-comments
-  // @TODO turn minify back on after verifying that everything works well
+  // @TODO turn minify back on once it stops removing 'use client'
   minify: false,
 
   rollup: {
