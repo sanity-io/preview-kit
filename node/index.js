53927208ce6025ab2a1378166822d20c0b1bf500
Cody Olsen <81981+stipsan@users.noreply.github.com>
2023-01-02 18:21:28 +0100

    fix: improve Node.js ESM runtime support

---
+++ node/index.js
@@ -0,0 +1,8 @@
+import cjs from '../dist/index.cjs'
+
+export const PreviewSuspense = cjs.PreviewSuspense
+export const _checkAuth = cjs._checkAuth
+export const _definePreview = cjs._definePreview
+export const _lazyEventSourcePolyfill = cjs._lazyEventSourcePolyfill
+export const _lazyGroqStore = cjs._lazyGroqStore
+export const definePreview = cjs.definePreview
