d947f3cfff484fd8e43519012bd8ebd91266a63c
Marius Lundgård <studio@mariuslundgard.com>
2023-01-03 11:33:44 +0100

    fix(build): auto-generate Node ESM wrapper

--- node/index.js
+++
@@ -1,8 +0,0 @@
-import cjs from '../dist/index.cjs'
-
-export const PreviewSuspense = cjs.PreviewSuspense
-export const _checkAuth = cjs._checkAuth
-export const _definePreview = cjs._definePreview
-export const _lazyEventSourcePolyfill = cjs._lazyEventSourcePolyfill
-export const _lazyGroqStore = cjs._lazyGroqStore
-export const definePreview = cjs.definePreview
