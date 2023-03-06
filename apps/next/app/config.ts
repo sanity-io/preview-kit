0eb4a8aba20743545b43cce9c15ea84e9235a2fe
Cody Olsen <stipsan@gmail.com>
2023-03-06 02:03:40 +0100

    chore: fix typo

--- apps/next/app/config.ts
+++ apps/next/app/config.ts
@@ -1,8 +1,7 @@
 /* eslint-disable no-process-env */
 
-export const projectId =
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'production'
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'pv8y60vp'
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'pv8y60vp'
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2022-11-15'
 export const useCdn = false
