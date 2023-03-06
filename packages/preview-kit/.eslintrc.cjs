51604e1a82811217a1c158d154d9db1045b9907b
Cody Olsen <stipsan@gmail.com>
2023-03-06 01:10:21 +0100

    chore: fix linting

--- packages/preview-kit/.eslintrc.cjs
+++ packages/preview-kit/.eslintrc.cjs
@@ -8,12 +8,13 @@ module.exports = {
     'sanity/react',
     'sanity/typescript',
     '@sanity/eslint-config-studio',
-    'next/core-web-vitals',
+    'plugin:@next/next/recommended',
     'plugin:prettier/recommended',
   ],
   rules: {
     'simple-import-sort/imports': 'warn',
     'simple-import-sort/exports': 'warn',
     'react-hooks/exhaustive-deps': 'error',
+    '@next/next/no-html-link-for-pages': 'off',
   },
 }
