55188fea2a23fa54c600086f64dbeb9c8c257711
Cody Olsen <cody@sanity.io>
2023-05-24 04:30:19 +0200

    fix: improve Portable Text support (#349)

--- packages/preview-kit/src/client/transcode.ts
+++ packages/preview-kit/src/client/transcode.ts
@@ -24,9 +24,37 @@ const filterDefault: FilterDefault = ({ path }) => {
     return false
   }
 
-  // Don't encode style keys for portable text
-  if (typeof path.at(-2) === 'number' && endPath === 'style') {
+  /**
+   * Best effort infer Portable Text paths that should not be encoded.
+   * Nothing is for certain, and the below implementation may cause paths that aren't Portable Text and otherwise be safe to encode to be skipped.
+   * However, that's ok as userland can always opt-in with the `encodeSourceMapAtPath` option and mark known safe paths as such, which will override this heuristic.
+   */
+  // If the path ends in [number].children[number].marks[number] it's likely a PortableTextSpan: https://github.com/portabletext/types/blob/e54eb24f136d8efd51a46c6a190e7c46e79b5380/src/portableText.ts#LL154C16-L154C16
+  if (
+    typeof endPath === 'number' &&
+    path.at(-2) === 'marks' &&
+    typeof path.at(-3) === 'number' &&
+    path.at(-4) === 'children' &&
+    typeof path.at(-5) === 'number'
+  ) {
     return false
+  }
+  // Or if it's [number].markDefs[number].href it's likely a PortableTextLink: https://github.com/portabletext/types/blob/e54eb24f136d8efd51a46c6a190e7c46e79b5380/src/portableText.ts#L163
+  if (
+    endPath === 'href' &&
+    typeof path.at(-2) === 'number' &&
+    path.at(-3) === 'markDefs' &&
+    typeof path.at(-4) === 'number'
+  ) {
+    return false
+  }
+  // Otherwise we have to deal with special properties of PortableTextBlock, and we can't confidently know if it's actually a `_type: 'block'` array item or not.
+  // All we know is that if it is indeed a block, and we encode the strings on these keys it'll for sure break the PortableText rendering and thus we skip encoding.
+  if (typeof endPath === 'string' && typeof path.at(-2) === 'number') {
+    // https://github.com/portabletext/types/blob/e54eb24f136d8efd51a46c6a190e7c46e79b5380/src/portableText.ts#L48-L58
+    if (endPath === 'style' || endPath === 'listItem') {
+      return false
+    }
   }
 
   return true
