d3e56cfc3b68e28b42baa43dbf86182f552e0de3
Cody Olsen <stipsan@gmail.com>
2023-06-10 02:27:45 +0200

    chore: disable `live-store`

--- packages/preview-kit/test.mjs
+++ packages/preview-kit/test.mjs
@@ -5,9 +5,9 @@ import { strict as assert } from 'node:assert'
 import { useListeningQuery } from '@sanity/preview-kit'
 import { createClient } from '@sanity/preview-kit/client'
 import { GroqStoreProvider } from '@sanity/preview-kit/groq-store'
-import { LiveStoreProvider } from '@sanity/preview-kit/live-store'
+// import { LiveStoreProvider } from '@sanity/preview-kit/live-store'
 
 assert.equal(typeof useListeningQuery, 'function')
 assert.equal(typeof createClient, 'function')
 assert.equal(typeof GroqStoreProvider, 'object')
-assert.equal(typeof LiveStoreProvider, 'object')
+// assert.equal(typeof LiveStoreProvider, 'object')
