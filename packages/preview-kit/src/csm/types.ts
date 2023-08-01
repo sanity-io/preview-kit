8087bd09bff21e71a5e8c4c1cc2c046fb5ef36d6
Michael Wain <michael@sanity.io>
2023-08-01 19:08:25 +0100

    docs: adds CSM section

--- packages/preview-kit/src/csm/types.ts
+++ packages/preview-kit/src/csm/types.ts
@@ -1,6 +1,43 @@
 import type { ContentSourceMap } from '@sanity/client'
 
 /** @public */
+export interface CreateTranscoderConfig {
+  /**
+   * Where the Studio is hosted.
+   * If it's embedded in the app, use the base path for example `/studio`.
+   * Otherwise provide the full URL to where the Studio is hosted, for example: `https://blog.sanity.studio`.
+   *
+   * @alpha
+   */
+  studioUrl: StudioUrl
+
+  /**
+   * Some strings shouldn't be encoded, for example if you store internal URLs the stega characters will break routing.
+   * ```ts
+    {
+      encodeSourceMapAtPath: (props) => {
+        switch (props.path.at(-1)) {
+          case 'url':
+            return false
+          default:
+            return props.filterDefault(props)
+        }
+      }
+    }
+   * ```
+   * @alpha
+   */
+  encodeSourceMapAtPath?: FilterDefault
+
+  /**
+   * Specify a `console.log` compatible logger to see debug logs, which keys are encoded and which are not.
+   *
+   * @alpha
+   */
+  logger?: Logger
+}
+
+/** @public */
 export type PathSegment = string | number
 
 /** @public */
