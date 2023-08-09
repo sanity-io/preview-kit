e89f604afada985d49508dcafc123985430005a4
Cody Olsen <stipsan@gmail.com>
2023-08-09 03:50:06 +0200

    chore: test iframe pane

--- apps/studio/sanity.config.ts
+++ apps/studio/sanity.config.ts
@@ -1,14 +1,41 @@
 import { groqdPlaygroundTool } from 'groqd-playground'
 
 import { visionTool } from '@sanity/vision'
-import { defineConfig, defineField, defineType } from 'sanity'
+import { defineConfig, defineField, defineType,SanityDocumentLike } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { benchmarkTool } from './src/benchmark'
+import { IframeOptions, Iframe } from 'sanity-plugin-iframe-pane'
+
+
+let defineGetPreviewUrl = (base: string) => function getPreviewUrl(doc: SanityDocumentLike) {
+  const url = new URL(base)
+  if(doc?._rev) {
+    url.searchParams.set('rev', doc._rev)
+  }
+  return url.toString()
+}
+
 
 const config = defineConfig({
   projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
   dataset: process.env.SANITY_STUDIO_DATASET!,
-  plugins: [deskTool(), benchmarkTool(), visionTool(), groqdPlaygroundTool()],
+  plugins: [deskTool({defaultDocumentNode: (S, {schemaType}) => {
+    // Only show preview pane on `movie` schema type documents
+    switch (schemaType) {
+      case `page`:
+        return S.document().views([
+          S.view.form(),
+          S.view
+            .component(Iframe)
+            .options({
+              url: defineGetPreviewUrl('https://preview-kit-next-app-router.sanity.build'),
+            } satisfies IframeOptions)
+            .title('next-app-router'),
+        ])
+      default:
+        return S.document().views([S.view.form()])
+    }
+  }}), benchmarkTool(), visionTool(), groqdPlaygroundTool()],
   schema: {
     types: [
       defineType({
