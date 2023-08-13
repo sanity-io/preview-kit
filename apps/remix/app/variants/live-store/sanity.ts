0d24db35e743ac0a42a10177d5d9cac5b7daf2ce
Cody Olsen <81981+stipsan@users.noreply.github.com>
2023-08-13 18:25:44 +0200

    chore: fix remix build

--- apps/remix/app/variants/live-store/sanity.ts
+++ apps/remix/app/variants/live-store/sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@sanity/preview-kit/client'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'pv8y60vp'
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2022-11-15'
+const projectId = 'pv8y60vp'
+const dataset = 'production'
+const apiVersion = '2022-11-15'
 const useCdn = false
 const studioUrl = 'https://preview-kit-test-studio.sanity.build/'
 export const client = createClient({
