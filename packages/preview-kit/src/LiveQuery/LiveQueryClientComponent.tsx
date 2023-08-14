9863ff0b1837ec5894c3c75547fc6e5c5095e0e3
Cody Olsen <stipsan@gmail.com>
2023-08-14 13:10:08 +0200

    chore: remove unused directive

--- packages/preview-kit/src/LiveQuery/LiveQueryClientComponent.tsx
+++ packages/preview-kit/src/LiveQuery/LiveQueryClientComponent.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import type { QueryParams as ClientQueryParams } from '@sanity/client'
 import type { PropsWithChildren } from 'react'
 import { Children, cloneElement, isValidElement } from 'react'
