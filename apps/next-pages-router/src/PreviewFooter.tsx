25aed5485a8b92e4dbf50df382f9ba4da66103f6
Cody Olsen <stipsan@gmail.com>
2023-08-17 20:41:15 +0200

    chore: cleanup

--- apps/next-pages-router/src/PreviewFooter.tsx
+++
@@ -1,8 +0,0 @@
-import { FooterProps, footerQuery, Footer } from 'ui/react'
-import { useLiveQuery } from '@sanity/preview-kit'
-
-export default function PreviewFooter(props: FooterProps) {
-  const [data] = useLiveQuery<FooterProps['data']>(props.data, footerQuery)
-
-  return <Footer data={data} />
-}
--- apps/next-pages-router/src/PreviewTable.tsx
+++
@@ -1,8 +0,0 @@
-import { TableProps, tableQuery, Table } from 'ui/react'
-import { useLiveQuery } from '@sanity/preview-kit'
-
-export default function PreviewTable(props: TableProps) {
-  const [data] = useLiveQuery<TableProps['data']>(props.data, tableQuery)
-
-  return <Table data={data} />
-}
