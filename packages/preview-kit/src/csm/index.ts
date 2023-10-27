0562ccd861e7bdea7bdc7a4eae39b446a3236911
Cody Olsen <stipsan@gmail.com>
2023-10-27 17:35:06 +0200

    feat: use CSM utils exported by `@sanity/client`

--- packages/preview-kit/src/csm/index.ts
+++
@@ -1,6 +0,0 @@
-export { encodeJsonPathToUriComponent } from './editIntent'
-export { parseNormalisedJsonPath } from './jsonpath'
-export * from './mapToEditLinks'
-export { resolveMapping, walkMap, type WalkMapFn } from './sourcemap'
-export * from './transcode'
-export type * from './types'
