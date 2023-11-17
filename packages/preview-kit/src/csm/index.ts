987fe1c45965483386ab8e2f42334da8d7e2eab4
Cody Olsen <stipsan@gmail.com>
2023-11-17 03:50:03 +0100

    fix: stop exporting internal `resolveMapping` API

--- packages/preview-kit/src/csm/index.ts
+++ packages/preview-kit/src/csm/index.ts
@@ -1,4 +1,4 @@
 export * from './mapToEditLinks'
-export { resolveMapping, walkMap, type WalkMapFn } from './sourcemap'
+export { walkMap, type WalkMapFn } from './sourcemap'
 export * from './transcode'
 export type * from './types'
