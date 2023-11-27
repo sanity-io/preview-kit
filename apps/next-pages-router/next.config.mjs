d5664ef1a78e70ce46ffc27e695e89117a892798
Cody Olsen <stipsan@gmail.com>
2023-11-27 21:56:44 +0100

    chore: test X-Frame-Options workaround

--- apps/next-pages-router/next.config.mjs
+++ apps/next-pages-router/next.config.mjs
@@ -8,6 +8,24 @@ const nextConfig = {
     },
   },
   productionBrowserSourceMaps: true,
+  // Support Vercel Authentication protection
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: `frame-ancestors 'self' https://*.sanity.build`,
+          },
+        ],
+      },
+    ];
+  }
 }
 
 export default withBundleAnalyzer({
