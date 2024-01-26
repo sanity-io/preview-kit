ee98027bcfa9aea0a12651ab4874f0c22168b7d6
Cody Olsen <stipsan@gmail.com>
2024-01-26 23:46:02 +0100

    chore: fix warn

--- apps/next-app-router/next.config.mjs
+++ apps/next-app-router/next.config.mjs
@@ -8,7 +8,6 @@ const nextConfig = {
     },
   },
   reactStrictMode: true,
-  serverActions: true,
   productionBrowserSourceMaps: true,
   // Support Vercel Authentication protection
   async headers() {
