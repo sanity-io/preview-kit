0647fef1782ea44bbc7ef58d80ff04947f29536d
Cody Olsen <stipsan@gmail.com>
2024-10-14 23:56:32 +0200

    fix: increase next v15 compat

--- packages/preview-kit/src/index.ts
+++ packages/preview-kit/src/index.ts
@@ -1,6 +1,5 @@
 export {type isEqualFn, type LiveQueryHookOptions, useIsEnabled, useLiveQuery} from './hooks'
 export {
-  createLiveQueryProvider,
   /**
    * Re-export to make `LiveQueryProvider` easier to use with `React.lazy`
    * @public
