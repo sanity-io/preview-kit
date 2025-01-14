6e7ca320d92790e84be2dd67c36c1f45ec487c38
Cody Olsen <stipsan@gmail.com>
2025-01-14 12:30:56 +0100

    chore: simplify

--- apps/studio/sanity.config.ts
+++ apps/studio/sanity.config.ts
@@ -5,8 +5,7 @@ import {defineConfig, defineField, defineType} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {benchmarkTool} from './src/benchmark'
 import {IframeOptions, Iframe} from 'sanity-plugin-iframe-pane'
-import {presentationTool as stablePresentationTool} from 'sanity/presentation'
-import {presentationTool as experimentalPresentationTool} from '@sanity/presentation'
+import {presentationTool} from 'sanity/presentation'
 
 const iframeOptions = {
   reload: {
@@ -26,85 +25,76 @@ const iframes: [string, string][] = [
   'remix-live-store',
 ].map((title) => [`https://preview-kit-${title}.sanity.dev`, title])
 
-function createConfig(stable: boolean) {
-  const name = stable ? 'stable' : 'experimental'
-  const presentationTool = stable ? stablePresentationTool : experimentalPresentationTool
-  const config = defineConfig({
-    name,
-    basePath: `/${name}`,
-    projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
-    dataset: process.env.SANITY_STUDIO_DATASET!,
-    plugins: [
-      deskTool({
-        defaultDocumentNode: (S, {schemaType}) => {
-          switch (schemaType) {
-            case `page`:
-              return S.document().views([
-                S.view.form(),
-                ...iframes.map(([url, title]) =>
-                  S.view
-                    .component(Iframe)
-                    .options({
-                      ...iframeOptions,
-                      key: title,
-                      url,
-                    } satisfies IframeOptions)
-                    .title(title),
-                ),
-              ])
-            default:
-              return S.document().views([S.view.form()])
-          }
+export default defineConfig({
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
+  dataset: process.env.SANITY_STUDIO_DATASET!,
+  plugins: [
+    deskTool({
+      defaultDocumentNode: (S, {schemaType}) => {
+        switch (schemaType) {
+          case `page`:
+            return S.document().views([
+              S.view.form(),
+              ...iframes.map(([url, title]) =>
+                S.view
+                  .component(Iframe)
+                  .options({
+                    ...iframeOptions,
+                    key: title,
+                    url,
+                  } satisfies IframeOptions)
+                  .title(title),
+              ),
+            ])
+          default:
+            return S.document().views([S.view.form()])
+        }
+      },
+    }),
+    presentationTool({
+      name: 'remix',
+      previewUrl: {
+        origin: process.env.SANITY_STUDIO_REMIX_URL || 'http://localhost:3002',
+        previewMode: {
+          enable: '/api/draft',
         },
-      }),
-      presentationTool({
-        name: 'remix',
-        previewUrl: {
-          origin: process.env.SANITY_STUDIO_REMIX_URL || 'http://localhost:3002',
-          previewMode: {
-            enable: '/api/draft',
-          },
+      },
+    }),
+    presentationTool({
+      name: 'pages-router',
+      previewUrl: {
+        origin: process.env.SANITY_STUDIO_PAGES_ROUTER_URL || 'http://localhost:3000',
+        previewMode: {
+          enable: '/api/draft',
         },
-      }),
-      presentationTool({
-        name: 'pages-router',
-        previewUrl: {
-          origin: process.env.SANITY_STUDIO_PAGES_ROUTER_URL || 'http://localhost:3000',
-          previewMode: {
-            enable: '/api/draft',
-          },
+      },
+    }),
+    presentationTool({
+      name: 'app-router',
+      previewUrl: {
+        origin: process.env.SANITY_STUDIO_APP_ROUTER_URL || 'http://localhost:3001',
+        previewMode: {
+          enable: '/api/draft',
         },
+      },
+    }),
+    benchmarkTool(),
+    visionTool(),
+    groqdPlaygroundTool(),
+  ],
+  schema: {
+    types: [
+      defineType({
+        name: 'page',
+        title: 'Page',
+        type: 'document' as const,
+        fields: [
+          defineField({
+            name: 'title',
+            type: 'string',
+          }),
+        ],
       }),
-      presentationTool({
-        name: 'app-router',
-        previewUrl: {
-          origin: process.env.SANITY_STUDIO_APP_ROUTER_URL || 'http://localhost:3001',
-          previewMode: {
-            enable: '/api/draft',
-          },
-        },
-      }),
-      benchmarkTool(),
-      visionTool(),
-      groqdPlaygroundTool(),
     ],
-    schema: {
-      types: [
-        defineType({
-          name: 'page',
-          title: 'Page',
-          type: 'document' as const,
-          fields: [
-            defineField({
-              name: 'title',
-              type: 'string',
-            }),
-          ],
-        }),
-      ],
-    },
-  })
-  return config
-}
-
-export default [createConfig(true), createConfig(false)]
+  },
+})
