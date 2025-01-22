26b4782f476735ebf50c78598061a0c24e0353bf
Cody Olsen <stipsan@gmail.com>
2025-01-22 18:01:17 +0100

    chore: handle draft mode with bypass support

--- apps/next-pages-router/src/app/api/draft/route.ts
+++ apps/next-pages-router/src/app/api/draft/route.ts
@@ -1,6 +1,6 @@
 import {defineEnableDraftMode} from 'next-sanity/draft-mode'
-import {client} from '../../sanity.client'
-import {token} from '../../sanity.fetch'
+import {client} from '../../../sanity.client'
+import {token} from '../../../sanity.fetch'
 
 export const {GET} = defineEnableDraftMode({
   client: client.withConfig({token}),
--- apps/next-pages-router/src/pages/api/draft.ts
+++
@@ -1,7 +0,0 @@
-import type {NextApiRequest, NextApiResponse} from 'next'
-
-export default function preview(req: NextApiRequest, res: NextApiResponse): void {
-  res.setDraftMode({enable: true})
-  res.writeHead(307, {Location: '/'})
-  res.end()
-}
